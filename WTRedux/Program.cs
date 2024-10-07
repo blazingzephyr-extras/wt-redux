e457f9927401d3bb8d874c10a9a702450033a69f
blazingzephyr <blazingzephyr@proton.me>
2024-10-07 14:06:55 +0700

    Initial commit
    Included FNA and ecslite.

---
+++ WTRedux/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Xna.Framework;
+
+namespace WTRedux
+{
+    internal class Program
+    {
+        void Main()
+        {
+            Game game = new Game();
+            game.Run();
+        }
+    }
+}
